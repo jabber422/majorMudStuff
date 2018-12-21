238114d315b0c3632712bc5d607711f5b10a420a
jabber422 <34468769+jabber422@users.noreply.github.com>
2018-12-20 16:46:24 -0800

    mmud connection project

---
+++ MMudTerm_Connection/IConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMudTerm_Connection
+{
+    public delegate void RcvMsgCallback(byte[] buffer);
+
+    public interface IConnection
+    {
+        bool Connect();
+        bool Disconnect();
+
+        bool Connected {get;}
+        
+        event RcvMsgCallback Rcvr;
+        event EventHandler Disconnected;
+        int Send(byte[] buffer);
+        
+    }
+}
